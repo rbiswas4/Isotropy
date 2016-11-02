0dbef6eea26da86056542d764894d8b8e6c217d9
R. Biswas <rbiswas4@gmail.com>
2016-11-02 10:55:45 -0700

    Adding install scripts 	new file:   install_miniconda.sh 	new file:   requirements.txt 	new file:   setup_env.sh

---
+++ install/setup_env.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+# store_dir is the directory where the miniconda install is placed
+export store_dir=${HOME}/store
+export MINICONDA_VERSION=${MINICONDA_VERSION:-"latest"}
+export CACHE_DIR="${store_dir}/miniconda.tarball"
+export CACHE_DIR_TMP="$CACHE_DIR.tmp"
+export CACHE_TARBALL_NAME="miniconda.tar.gz"
+export CACHE_TARBALL_PATH="$CACHE_DIR/$CACHE_TARBALL_NAME"
+export PATH="${store_dir}/miniconda/bin:$PATH"
+
