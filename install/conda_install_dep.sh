9f5950dfac643d985e4cdcd2d4c844478ac33ae8
R. Biswas <rbiswas4@gmail.com>
2016-11-02 10:59:19 -0700

    Adding dependency install 	new file:   conda_install_dep.sh

---
+++ install/conda_install_dep.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+export PATH="${store_dir}/miniconda/bin:$PATH"
+#CHANNEL=${CHANNEL:-"http://conda.lsst.codes/sims"} # the URL to the conda channel where LSST conda packages reside
+#conda config --add channels "$CHANNEL"
+# CHANNEL=${CHANNEL:-"pandas"}
+#conda config --add channels "$CHANNEL"
+#CHANNEL=${CHANNEL:-"astropy"}
+conda config --add channels pandas 
+conda config --add channels astropy
+conda config --add channels zonca
+# conda create --yes -n AnalyzeSN python
+# source activate AnalyzeSN
+conda install -q --yes --file ./install/requirements.txt
+conda list --explicit > spec-file.txtconda list --explicit > ./install/spec-file.txt;
+
