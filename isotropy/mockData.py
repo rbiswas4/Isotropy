f29bb60744e96a576fdd66a729ce9f66e246576f
R. Biswas <rbiswas4@gmail.com>
2016-12-03 03:09:39 -0800

    Added function drawSamples with correct docstrings 	modified:   mockData.py

--- isotropy/mockData.py
+++ isotropy/mockData.py
@@ -21,8 +21,9 @@ import gzip
 import pickle
 import pandas as pd
 import numpy as np
+from astropy.cosmology import Planck15
 
-__all__ = ['read_mockDataPickle', 'binnedDescStat']
+__all__ = ['read_mockDataPickle', 'binnedDescStat', 'drawSamples']
 
 def read_mockDataPickle(fname, filterBadPoints=True, selectCols=('z', 'mu', 'mu_var')):
     """
@@ -44,25 +45,39 @@ def read_mockDataPickle(fname, filterBadPoints=True, selectCols=('z', 'mu', 'mu_
         snFits = pickle.load(gzip.GzipFile(fname),
                                            encoding='latin1')
     df = pd.DataFrame(snFits).transpose()
+    totalSN = len(df)
     if filterBadPoints:
         df = df.query('mu < 19. and mu > 0.')
 
     if selectCols is not None:
         df = df[list(selectCols)].astype(np.float)
-    return df
+    return df, totalSN
 
 def binnedDescStat(mockDataFrame,
-                                binningCol='z',
-                                varColumn='mu_err',
-                                outlier_rejection_query='mu_err < 5.0',
-                                statisticsTuple=('count', np.mean, np.std),
-                                binwidth=0.1):
+                   binningCol='z',
+                   varColumn='mu_err',
+                   outlier_rejection_query='mu_err < 5.0',
+                   statisticsTuple=('count', np.mean, np.std),
+                   binwidth=0.1):
     """
     bin an input dataFrame with columns of `z` and `mu_var` in uniform redshift
     bins of width 0.1, rejecting outliers in `mu_var` via a simple prescription
     described below. Return descriptive statistics of the uncertainties in
     distance modulus in each bin in the form of a mean and standard deviation of
     a Gaussian.
+
+    Parameters
+    ----------
+    mockDataFrame : `pd.DataFrame`
+    binningCol : string, optional, defaults to `z`
+    varColumn : string, optional, defaults to `mu_err`
+    outlier_rejection_query : string, optional defaults to vals below 5.0
+    statisticsTuple : tuple, optional, defaults to ('count', np.mean, np.std)
+    binwidth : float, default to 0.1
+
+    Returns
+    -------
+    dataFrame
     """
     # Since we will be modifying the dataframe, create copy
     df  = mockDataFrame.copy(deep=True)
@@ -75,7 +90,7 @@ def binnedDescStat(mockDataFrame,
     df.binindex = df.binindex.astype(int)
 
     # Bin using groupby and aggregate to find descriptive statistics
-    grouped = df.groupby('binindex') 
+    grouped = df.groupby('binindex')
 
     # statistics of interest : How do we calculate them from columns of
     # dataframe. In this case, we use a single column of the dataframe 'mu_err'
@@ -83,5 +98,64 @@ def binnedDescStat(mockDataFrame,
     statisticsDict = dict(mu_err=statisticsTuple)
 
     # aggregate on the grouped object to obtain the statistics in each bin
-    return grouped.agg(statisticsDict)
+    return grouped.agg(statisticsDict)[varColumn] 
+
+def drawSamples(data, numSN, totalSN, rng, binwidth=0.1, minz=0., cosmo=Planck15):
+    """
+    Draw samples of z, mu, mu_err from the binned descriptive statisics `data`
+
+    Parameters
+    ----------
+    data : `pd.DataFrame`, mandatory
+        table with counts, mean(mu_err), and std(mu_err) in columns
+    numSN : int, mandatory
+        total number of SN in the mock data (many of these will be bad data
+        points) so the total number expected is smaller 
+    totalSN : int, mandatory
+        total number of SN from which `data`  parameter was derived
+    rng : instance of `numpy.random.RandomState`
+        random number state used to draw the statistical distributions
+    binwidth : float, optional, defaults to 0.1
+        width of (assumed) uniform redshift bins
+    minz : float, optional, defaults to 0.
+        min redshift used in the data
+    cosmo : instance of cosmological models in astropy.cosmology, defaults to Planck15
+        cosmological model used to obtain the uncertainties in distance modulus
+    """
+    data['frequency'] = data['count'] / np.float(totalSN)
+    data['numexpected'] = np.round(data['frequency'].values * numSN).astype(np.int)
+
+    # The total number of objects
+    numExpTot = data.numexpected.sum()
+    
+    # Initialize arrays with random samples
+    z = rng.uniform(0., binwidth, size=numExpTot)
+    mu_err = rng.normal(0., 1., size=numExpTot)
+    scatter = rng.normal(0., 1.0, size=numExpTot)
+
+    # use linear functions to change samples to have appropriate distributions
+    low = 0
+    for idx, row in data.iterrows():
+        # to slice the numpy arrays we use the count in each bin
+        high = low + row['numexpected'].astype(int)
+
+        # to get redshift, add the minz, and idx * binwidth as upper limit
+        z[low:high] += minz + idx * binwidth
+
+        # For mu_err first rescale the standard deviation then add to mean
+        mu_err[low:high] *= row['std']
+        mu_err[low:high] += row['mean']
+
+        # reset low for next iteration
+        low = high
+        
+        # distance moduli mu
+        # We use the cosmology to calculate distance moduli corresponding to the z
+        # Add a scatter consistent with mu_err
+        mu = cosmo.distmod(z).value + scatter * mu_err
+
+        # return as a dataframe
+        df = pd.DataFrame(dict(z=z, mu=mu, mu_err=mu_err))
+
+    return df
 
